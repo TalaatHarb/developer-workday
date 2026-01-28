19b2d6d52c08001a00d2946256c18fbaa505811d
mharb <mohamed.harb@cegedim.com>
2026-01-28 02:21:47 +0200

    Running application

--- developer-workday/src/main/java/net/talaatharb/workday/DeveloperWorkdayApplication.java
+++ developer-workday/src/main/java/net/talaatharb/workday/DeveloperWorkdayApplication.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class DevloperWorkdayApplication{
+public class DeveloperWorkdayApplication{
 	public static void main(String[] args) {
 		log.info("UI Application Starting");
 		Application.launch(JavafxApplication.class, args);
